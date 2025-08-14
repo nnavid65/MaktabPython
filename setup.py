657d780f7c00edbfefbdd944a0356f2c1bf4938c
nnavid65 <nnavid65@outlook.com>
2025-08-14 19:58:09 +0200

    create a setup file to install package library with pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mylibrary",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    description="A simple test library",
+    author="Your Name",
+    author_email="you@example.com",
+    license="MIT"
+)
